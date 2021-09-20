b0eb5b4af485902ead337e83f8211a867c96e42d
user0028 <89523119+user0028@users.noreply.github.com>
2021-09-21 04:58:18 +0530

    Add files via upload

---
+++ week9HW.js
@@ -0,0 +1,4 @@
+var vg_1 = "week9HW.vg.json";
+vegaEmbed("#asia_deaths", vg_1).then(function(result) {
+ // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
